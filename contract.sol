2d961aff69298b715cebb71b4ace9924242cb64d
Nayeon <81788398+XiJiaoBu@users.noreply.github.com>
2024-11-17 08:36:28 +0700

    update

--- contract.sol
+++ contract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.28;
 
 contract VideoMinting {
     // This event is emitted when a video is minted, recording the user and the video hash.
